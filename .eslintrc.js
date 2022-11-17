8b4fd58b39c1f64c2d296a67bfa61bfc6c9ba55c
adxxtya <andronaut9@gmail.com>
2022-11-17 11:51:51 +0530

    fixing bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,9 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-this-alias": "off",
+        
     }
 }
