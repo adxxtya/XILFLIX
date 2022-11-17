0df1fd9e70fbe5bae3dc6fedaa1ee5b3e01433a8
adxxtya <andronaut9@gmail.com>
2022-11-17 12:33:35 +0530

    fixing bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     "extends": [
         // "eslint:recommended",
         "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react/tsx-runtime",
         // "plugin:@typescript-eslint/recommended",
         "next/core-web-vitals"
     ],
@@ -23,6 +25,7 @@ module.exports = {
     "rules": {
         "jsx-a11y/alt-text": [0],
         "react/react-in-jsx-scope": "off",
+        "react/react-in-tsx-scope": "off",
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx",".ts", ".tsx"] }],
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
