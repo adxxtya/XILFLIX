9b89e5a6ad72fd5bdc66627d33b759f57ca6abd8
adxxtya <andronaut9@gmail.com>
2022-11-17 11:30:15 +0530

    fixing bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
+        "eslint:recommended",
         "plugin:react/recommended",
-        "google"
+        "plugin:@typescript-eslint/recommended",
+        "next/core-web-vitals"
     ],
     "overrides": [
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "@typescript-eslint"
     ],
     "rules": {
     }
