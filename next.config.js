1af13ed4184f7d5f07dc34f3478f6b01fbef34cc
adxxtya <andronaut9@gmail.com>
2022-11-17 11:18:11 +0530

    fixing bugs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withTM = require('next-transpile-modules')([
 ]) // pass the modules you would like to see transpiled
 
 module.exports = withTM({
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: ['rb.gy', 'image.tmdb.org'],
   },
